ddea900560089f96892f0fc16ddc9aec42e8f88e
winter <dong.cb@teko.vn>
2024-05-29 11:29:56 +0700

    1208 get equal substring with budget

---
+++ 1208_get_equal_substrings_within_budget/main.py
@@ -0,0 +1,33 @@
+""" 
+Desceiption: Find the longest substring of s that can be changed to t with at most maxCost cost
+Ref: https://leetcode.com/problems/get-equal-substrings-within-budget/discuss/392918/JavaC%2B%2BPython-Sliding-Window
+"""
+class Solution:
+    '''
+    Solution:
+    Sliding window
+    Get max substring length with cost <= maxCost
+    Calculate max_len for each end index
+    
+    Complexity:
+    Time complexity: O(n)
+    Space complexity: O(n)
+    '''
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        n = len(s)
+        cost = [0] * n
+        for i in range(n):
+            cost[i] = abs(ord(s[i]) - ord(t[i]))
+        
+        start = 0
+        current_cost = 0
+        max_len = 0
+        for end in range(n):
+            current_cost += cost[end]
+            while current_cost > maxCost:
+                current_cost -= cost[start]
+                start += 1
+            max_len = max(max_len, end - start + 1)
+        return max_len
+
+print(Solution().equalSubstring('pxezla', 'loewbi', 25))
--- 131_palindrom_partioning/main.py
+++ 131_palindrom_partioning/main.py
@@ -1,40 +1,65 @@
 from typing import List
 
 
+# class Solution:
+#     def partition(self, s: str) -> List[List[str]]:
+
+#         n = len(s)
+#         map_palindrom = [[0] * n] * n
+#         result = []
+
+#         def is_palindrom(left, right):
+#             if left >= right:
+#                 return True
+#             if map_palindrom[left][right] == 0:
+#                 if s[left] != s[right]:
+#                     map_palindrom[left][right] == -1
+#                     return False
+#                 else:
+#                     return is_palindrom(left + 1, right - 1)
+#             else:
+#                 return map_palindrom[left][right]
+
+#         def backtrack(left, right, current):
+#             # print(s[left:right], left, right, current)
+#             if left == right:
+#                 result.append(current[:])
+#                 return result
+
+#             for i in range(left, right):
+#                 if is_palindrom(left, i):
+#                     current.append(s[left:i + 1])
+#                     # print(current)
+#                     backtrack(i + 1, right, current)
+#                     current.pop()
+#         backtrack(0, n, [])
+
+#         return result
+
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-
         n = len(s)
-        map_palindrom = [[0] * n] * n
+        dp = [[0] * n for _ in range(n)]
         result = []
+        
+        for right in range(n):
+            for left in range (right + 1):
+                if s[left] == s[right] and (right - left < 2 or dp[left + 1][right - 1]):
+                    dp[left][right] = True
+                
+        def backtrack(start=0, current=[]):
+            if start == n:
+                result.append(current)
+                return
+            
+            for end in range(start, n):
+                if dp[start][end]:
+                    backtrack(end+ 1, current + [s[start:end + 1]])
+        
+        backtrack()  
+        return result 
+        
 
-        def is_palindrom(left, right):
-            if left >= right:
-                return True
-            if map_palindrom[left][right] == 0:
-                if s[left] != s[right]:
-                    map_palindrom[left][right] == -1
-                    return False
-                else:
-                    return is_palindrom(left + 1, right - 1)
-            else:
-                return map_palindrom[left][right]
-
-        def backtrack(left, right, current):
-            # print(s[left:right], left, right, current)
-            if left == right:
-                result.append(current[:])
-                return result
-
-            for i in range(left, right):
-                if is_palindrom(left, i):
-                    current.append(s[left:i + 1])
-                    # print(current)
-                    backtrack(i + 1, right, current)
-                    current.pop()
-        backtrack(0, n, [])
-
-        return result
+print(Solution().partition("aab"))
 
 
-print(Solution().partition("a"))
